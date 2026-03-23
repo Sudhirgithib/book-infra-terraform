acc66957da64e07c902f767dcd878725765cf680
Sudhir <sudhirnalana04@gmail.com>
2026-03-23 10:42:53 +0530

    fixed backend only in root module

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
-
-  backend "s3" {
-  region = "us-east-1"
-}
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "book-infra-terraform-123"
+    key    = "book-infra/terraform.tfstate"
+    region = "us-east-1"
   }
 }
